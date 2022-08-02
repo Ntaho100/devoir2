a654cdc6c225a9e60304f23e38f3ef9da6a1d7c6
Ntaho100 <108214461+Ntaho100@users.noreply.github.com>
2022-08-02 13:27:26 +0000

    Add files via upload

---
+++ Devoir2/devoir1.js
@@ -0,0 +1,49 @@
+1.//trier le tableau par ordre croissant//
+
+let tab;
+tab=['bb','aa','nn','hh','xx'];
+
+console.log ('tableau original:',tab);
+console.log ('tableau trie:',tab.sort());
+
+
+let tab;
+tab=[0,8,9,5,608];
+
+console.log ('tableau original:',tab);
+console.log ('tableau trie:',tab.sort());
+
+2. //minimum et maximum//
+
+
+let tab;
+tab=[3,8,9,5,608];
+console.log("Min:",Math.min.apply(null,tab));
+console.log("Max:",Math.max.apply(null,tab));
+
+3.//nombre impairs //
+
+
+ let tab;
+ tab=[23,67,8,9,4,3,7,6,112,56,11,1,17];
+
+              if(tab%2 == 0)
+              {
+                     console.log("Nombre pair");
+              }
+              else
+              {
+                     console.log("Nombre impair");
+              }
+
+
+
+
+
+
+
+
+
+
+
+
